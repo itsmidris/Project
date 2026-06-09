20cafefdb73b05dc0858e0d3ab80b4252e0b633d
itsmidris <idrishiimran72@gmail.com>
2026-06-09 23:11:47 +0530

    Collection

--- LearnCollection/VectorDemo/VectorDemo.java
+++ LearnCollection/VectorDemo/VectorDemo.java
@@ -4,12 +4,40 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) {
+
         Vector<Integer> vector = new Vector<>();
+
+        // Add
         vector.add(10);
         vector.add(20);
         vector.add(30);
+        vector.add(20);
 
-        System.out.println(vector);
+        System.out.println("Vector : " + vector);
 
+        // Get
+        System.out.println("Element at index 1 : "
+                + vector.get(1));
+
+        // Update
+        vector.set(1, 99);
+        System.out.println("After Update : " + vector);
+
+        // Remove
+        vector.remove(2);
+        System.out.println("After Remove : " + vector);
+
+        // Contains
+        System.out.println("Contains 99 ? "
+                + vector.contains(99));
+
+        // Size
+        System.out.println("Size : " + vector.size());
+
+        // Loop
+        System.out.println("Elements:");
+        for (Integer num: vector){
+            System.out.println(num);
+        }
     }
 }
